2223b5a2e890e1ed7b9c2239ab44f4afde23706c
liem-khiet tran <liemkhiet.tran@gmail.com>
2016-01-11 16:11:18 +0400

    Initial Commit

---
+++ LKTNotificationBadge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LKTNotificationBadge
+//
+//  Created by liem-khiet tran on 1/11/16.
+//  Copyright © 2016 liem-khiet tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LKTNotificationBadge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LKTNotificationBadge
+//
+//  Created by liem-khiet tran on 1/11/16.
+//  Copyright © 2016 liem-khiet tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
